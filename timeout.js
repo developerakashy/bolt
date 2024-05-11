1934afa37a03c251a588a9e61c5fd5964cb2ef04
developerakashy <developerakashy@gmail.com>
2024-05-11 20:45:47 +0530

    time using js

---
+++ timeout.js
@@ -0,0 +1,32 @@
+function payment(callback, num){
+    const date_clicked = new Date()
+    console.log(date_clicked)
+
+    paymentstatus(num)
+
+    function paymentstatus(n){
+        let date_accessed = new Date(date_clicked)
+        let date_now = new Date()
+
+        if(date_now-date_accessed > n){
+            console.log(date_now)
+            console.log(Math.floor((date_now-date_accessed)/1000)+"s")
+            callback()
+            
+        }
+        else{
+            console.log("waiting...")
+            while (date_now-date_accessed <= n){
+                date_now = new Date()
+            }
+            paymentstatus(n)
+        }
+
+    }
+
+}
+
+
+payment(function (){
+    console.log("Payment Successfull")
+}, 5000)
